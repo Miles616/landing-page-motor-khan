66aa512ac535114a9aabe093253f90e6a9190a9f
Shahbaz Khan <shahbazkhan19117866@gmail.com>
2025-11-20 13:36:30 +0000

    i want more

--- src/components/ui/spotlight.tsx
+++ src/components/ui/spotlight.tsx
@@ -45,7 +45,7 @@ export const Spotlight = ({
           className
         )}
         style={{
-          backgroundImage: `radial-gradient(ellipse 100% 80% at 50% 120%, ${fill}, transparent 80%)`,
+          backgroundImage: `radial-gradient(ellipse 150% 100% at 50% 120%, ${fill}, transparent 70%)`,
         }}
       />
     );
