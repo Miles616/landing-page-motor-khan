326586fc2ef7e36cbe8c664f0e7c8d92bd084381
Shahbaz Khan <shahbazkhan19117866@gmail.com>
2025-11-22 16:32:52 +0000

    motor and khan do not have vertical space

--- src/components/ui/footer.tsx
+++ src/components/ui/footer.tsx
@@ -69,7 +69,7 @@ export default function Footer() {
               "--x": `${coords.x}px`,
               "--y": `${coords.y}px`,
               fontSize: 'clamp(4rem, 18vw, 12rem)',
-              lineHeight: 1.5,
+              lineHeight: 1.2,
               background: `radial-gradient(400px circle at var(--x) var(--y), hsl(0 0% 100%) 0%, hsla(0, 0%, 100%, 0.1) 25%, transparent 50%)`,
               backgroundClip: 'text',
               WebkitBackgroundClip: 'text',
